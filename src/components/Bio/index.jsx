d599d081669c95fbccd1032050e4683cbcb7d612
sk.ahm <itzshakeey@gmail.com>
2023-04-10 14:11:56 +0100

    navbar change

--- src/components/Bio/index.jsx
+++ src/components/Bio/index.jsx
@@ -3,7 +3,7 @@ import './Bio.css';
 export default function Bio({ name, bio }) {
   return (
     <div>
-      <h1>{name}</h1>
+
       <p role="summary">{bio}</p>
     </div>
   );
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,8 +6,8 @@ function Nav() {
             <h1>Elton John</h1>
             <ul className="nav-links">
                 <li>Home</li>
+                <li>Albums</li>
                 <li>About</li>
-                <li>Shop</li>
             </ul>
         </nav>
     )
