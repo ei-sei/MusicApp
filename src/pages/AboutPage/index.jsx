9941a42ce0bed84b390d402f2170c453353f9220
sk.ahm <itzshakeey@gmail.com>
2023-04-10 20:33:15 +0100

    add different pages

---
+++ src/pages/AboutPage/index.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This website allows you to look through fortnite unreleased upcoming items</p>
+      </div>
+    )
+  }
+    
+    export default About
+    
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>I am a homepage!</p>
+        </div>
+    )
+}
+
+export default Home;
